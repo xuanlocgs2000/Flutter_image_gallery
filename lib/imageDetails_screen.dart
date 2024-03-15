9d51cc146494c4bf95f97631f844bb9c6fcbd63b
xuanlocgs2000 <xuanlocboya@gmail.com>
2024-03-15 12:12:10 +0700

    zoom and handle error image

--- lib/imageDetails_screen.dart
+++ lib/imageDetails_screen.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class ImageDetailScreen extends StatefulWidget {
   final String imagePath;
   final List<String> imagePaths;
+  final int selectedIndex;
 
-  ImageDetailScreen(this.imagePath, this.imagePaths);
+  ImageDetailScreen(this.imagePath, this.imagePaths, this.selectedIndex);
 
   @override
   _ImageDetailScreenState createState() => _ImageDetailScreenState();
@@ -14,29 +15,37 @@ class _ImageDetailScreenState extends State<ImageDetailScreen> {
   late String currentImagePath;
   int selectedImageIndex = 0;
   final ScrollController _controller = ScrollController();
+  double _scale = 1.0;
+  double _previousScale = 5.0;
+
   @override
   void initState() {
     super.initState();
     currentImagePath = widget.imagePath;
+    selectedImageIndex = widget.selectedIndex;
   }
 
   void updateImagePath(String newPath, int index) {
     setState(() {
       currentImagePath = newPath;
       selectedImageIndex = index;
+
+      double offset =
+          index * 116.0 - MediaQuery.of(context).size.width / 2 + 65;
+
       _controller.animateTo(
-        index * (100), // Vị trí của ảnh được chọn
+        offset,
         duration: Duration(milliseconds: 500),
         curve: Curves.easeInOut,
       );
     });
   }
 
-  BoxDecoration getDecorationForImage(int index) {
+  BoxDecoration getDecorationForImage(int index, int currentIndex) {
     return BoxDecoration(
       borderRadius: BorderRadius.circular(8.0),
       border: Border.all(
-        color: index == selectedImageIndex
+        color: index == currentIndex
             ? Color.fromARGB(255, 6, 59, 195)
             : Colors.transparent,
         width: 3.0,
@@ -62,6 +71,16 @@ class _ImageDetailScreenState extends State<ImageDetailScreen> {
     }
   }
 
+  void _onScaleStart(ScaleStartDetails details) {
+    _previousScale = _scale;
+  }
+
+  void _onScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _scale = _previousScale * details.scale;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,14 +89,29 @@ class _ImageDetailScreenState extends State<ImageDetailScreen> {
       ),
       body: Column(
         children: [
-          Container(
+          _buildImageViewer(),
+          _buildImageSelector(),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildImageViewer() {
+    return Expanded(
+      child: Container(
+        child: InteractiveViewer(
+          boundaryMargin: EdgeInsets.all(200),
+          clipBehavior: Clip.hardEdge,
+          onInteractionStart: (details) => _onScaleStart(details),
+          onInteractionUpdate: (details) => _onScaleUpdate(details),
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height * 0.7,
             padding: EdgeInsets.symmetric(horizontal: 30),
             margin: EdgeInsets.symmetric(horizontal: 20),
-            width: 400,
-            height: 600,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage(currentImagePath),
+                image: NetworkImage(currentImagePath),
                 fit: BoxFit.cover,
               ),
               borderRadius: BorderRadius.circular(15),
@@ -105,42 +139,51 @@ class _ImageDetailScreenState extends State<ImageDetailScreen> {
               ],
             ),
           ),
-          SizedBox(
-            height: 10,
-          ),
-          Container(
-            height: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              controller: _controller,
-              itemCount: widget.imagePaths.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: GestureDetector(
-                    onTap: () {
-                      updateImagePath(widget.imagePaths[index], index);
-                    },
-                    child: Container(
-                      decoration: getDecorationForImage(index),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: Image.asset(
-                          widget.imagePaths[index],
-                          width: selectedImageIndex == index
-                              ? 130
-                              : 100, // zoom select
-                          height: selectedImageIndex == index ? 110 : 80,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildImageSelector() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: 100,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          controller: _controller,
+          itemCount: widget.imagePaths.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  updateImagePath(widget.imagePaths[index], index);
+                },
+                child: Container(
+                  decoration: getDecorationForImage(index, selectedImageIndex),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8.0),
+                    child: Image.network(
+                      widget.imagePaths[index],
+                      width: selectedImageIndex == index ? 130 : 100,
+                      height: selectedImageIndex == index ? 110 : 80,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Image.asset(
+                          'assets/image_error.png',
+                          width: 100,
+                          height: 200,
                           fit: BoxFit.cover,
-                        ),
-                      ),
+                        );
+                      },
                     ),
                   ),
-                );
-              },
-            ),
-          ),
-        ],
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,24 @@
 import 'package:flutter/material.dart';
 import 'imageDetails_screen.dart'; //
+import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 final List<String> imagePaths = [
-  "assets/img1.jpg",
-  "assets/img2.jpg",
-  "assets/img3.jpg",
-  "assets/img4.jpg",
-  "assets/img5.jpg",
-  "assets/img6.jpg",
+  "https://i.ibb.co/aaas",
+  "https://i.ibb.co/aaasq",
+  "https://i.ibb.co/aaasq2",
+  "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  // "https://images.pexels.com/ph1",
+  // "siduias",
+  '',
+  "https://i.ibb.co/vz6x9zK/VOZ-CNKT.png",
+  "https://i.ibb.co/vz6x9zK/VOZ-CNKT.png",
 ];
+
+final List<String> errorImagePaths = [];
 
 class MyApp extends StatelessWidget {
   @override
@@ -27,7 +33,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ImageListScreen extends StatelessWidget {
+class ImageListScreen extends StatefulWidget {
+  @override
+  _ImageListScreenState createState() => _ImageListScreenState();
+}
+
+class _ImageListScreenState extends State<ImageListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    errorImagePaths.addAll(imagePaths.where((path) => path.isEmpty));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +60,7 @@ class ImageListScreen extends StatelessWidget {
                 context,
                 MaterialPageRoute(
                   builder: (context) =>
-                      ImageDetailScreen(imagePaths[index], imagePaths),
+                      ImageDetailScreen(imagePaths[index], imagePaths, index),
                 ),
               );
             },
@@ -51,11 +68,36 @@ class ImageListScreen extends StatelessWidget {
               padding: EdgeInsets.all(8.0),
               child: ClipRRect(
                 borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  imagePaths[index],
-                  width: 100,
-                  height: 200,
-                  fit: BoxFit.cover,
+                child: CachedNetworkImage(
+                  imageUrl: imagePaths[index],
+                  progressIndicatorBuilder: (context, url, downloadProgress) =>
+                      CircularProgressIndicator(
+                          value: downloadProgress.progress),
+                  errorWidget: (context, url, error) => GestureDetector(
+                    // onTap: () {
+                    //   showDialog(
+                    //     context: context,
+                    //     builder: (context) => AlertDialog(
+                    //       title: Text('Error'),
+                    //       content: Text('Image not available.'),
+                    //       actions: [
+                    //         TextButton(
+                    //           onPressed: () {
+                    //             Navigator.of(context).pop();
+                    //           },
+                    //           child: Text('OK'),
+                    //         ),
+                    //       ],
+                    //     ),
+                    //   );
+                    // },
+                    child: Image.asset(
+                      "assets/image_error.png",
+                      width: 100,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
               ),
             ),
